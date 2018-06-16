f73a011332c3cbd1149490498ea7a13e021c56a9
dustfan <enioy74@gmail.com>
2018-06-17 00:57:39 +0800

    modify publicpath

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/F2E-1-todolist/docs/',
+    assetsPublicPath: '/F2E-2-filter/dist/',
 
     /**
      * Source Maps
