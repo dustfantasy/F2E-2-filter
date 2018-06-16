03c231abce6058f73a7600b83dbd13ab5228e855
dustfan <enioy74@gmail.com>
2018-06-17 01:47:59 +0800

    docs

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/F2E-2-filter/dist/',
+    assetsPublicPath: '/F2E-2-filter/docs/',
 
     /**
      * Source Maps
